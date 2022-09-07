9e2201f03fe5c5cfc6c483e83e96026d066a5ccb
dungbt-comartek <dungbt@comartek.com>
2022-09-07 16:57:12 +0700

    rÃefactor token service

--- src/controllers /auth.controller.js
+++ src/controllers /auth.controller.js
@@ -12,7 +12,7 @@ const register = async (req, res, next) => {
     const { email, password } = await validateUser.validateAsync(req.body);
     const users = await getUsers();
     // check email of user register
-    const emailIsExisted = users.some((user) => user.email === email);
+    const emailIsExisted = users.some( user => user.email === email);
     if (emailIsExisted)
       throw new Error(`email _ ${email} have been registed already !`);
     // encrypt password
@@ -38,15 +38,14 @@ const login = async (req, res, next) => {
     const { email, password } = await validateUser.validateAsync(req.body);
     // find user in db
     const users = await getUsers();
-    const userIsExisting = users.filter((user) => user.email === email)[0];
+    const userIsExisting = users.filter( user => user.email === email)[0];
     if (!userIsExisting) throw new Error(" email incorrect ");
     // decode password 
     const result = await decodePwd(password, userIsExisting.password);
     if (!result) throw new Error(" password incorrect ");
     // generate tokens
-    const { accessToken, refreshToken } = generateTokens({
-      email: email,
-    });
+    const accessToken = generateTokens({email : email}).accessToken()
+    const refreshToken = generateTokens({email : email}).accessToken()
     // store rf token in db of user
     await updateUser(userIsExisting, {
       ...userIsExisting,
@@ -72,11 +71,11 @@ const forgotPwd = async (req, res, next) => {
   try {
     // find user in db 
     const users = await getUsers();
-    const emailIsExisted = users.some((user) => user.email === email);
+    const emailIsExisted = users.some( user => user.email === email);
     if (!emailIsExisted)
       throw new Error(`email _ ${email} haven't been registed yet !`);
     // generate forgot token 
-    const forgotPwdToken = generateTokens({ email }, "forgotPwdToken");
+    const forgotPwdToken = generateTokens({ email : email }).forgotPwdToken()
     // send mail forgot pwd to user
     await sendMail({
       to: email,
@@ -100,10 +99,11 @@ const resetPwd = async (req, res, next) => {
   try {
     const forgotPwdToken = req.query.secret;
     // verify forgot password token 
-    verifyToken(forgotPwdToken, "forgotPwdToken");
+    const { email } = verifyToken(forgotPwdToken).forgotPwdToken();
     // response to client 
-    res.status(200).json({// validate user
+    res.status(200).json({
       success: true,
+      email : email , 
       forgotPwdToken: forgotPwdToken,
       url: `${hostUrl}/auth/updatePwd`,
     });
@@ -120,7 +120,7 @@ const updatePwd = async (req, res, next) => {
   const forgotPwdToken = authHeader && authHeader.split(" ")[1];
   try {
     // verify forgot password token 
-    const { email } = verifyToken(forgotPwdToken, "forgotPwdToken");
+    const { email } = verifyToken(forgotPwdToken).forgotPwdToken();
     // validate user
     const { password } = await validateUser.validateAsync({
       email: email,
@@ -128,7 +128,7 @@ const updatePwd = async (req, res, next) => {
     });
     //  find user 
     const users = await getUsers();
-    const user = users.filter((user) => user.email === email)[0];
+    const user = users.filter( user => user.email === email)[0];
     const newUser = JSON.parse(JSON.stringify(user));
     // encrypt password 
     const encryptedPwd = await encryptPwd(password);
--- src/services/token.service.js
+++ src/services/token.service.js
@@ -6,28 +6,36 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET ;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET ;
 const forgotPwdTokenSecret = process.env.FORGOT_PWD_TOKEN_SECRET ;
 
-
-const generateTokens = (payload , type) => {
-    if(type === "forgotPwdToken") return jwt.sign( payload , forgotPwdTokenSecret , {
-        expiresIn : "3h" ,
-    })
-    const accessToken = jwt.sign( payload , accessTokenSecret , {
-        expiresIn : "3h" ,
-    } ) ;
-    const refreshToken = jwt.sign( payload , refreshTokenSecret , {
-        expiresIn : "7d" ,
-    })
-    return { accessToken , refreshToken }
+const generateTokens = ( payload ) => {
+    return {
+        accessToken(){
+            return jwt.sign( payload , accessTokenSecret , {
+                expiresIn : "3d"
+            })
+        } ,
+        refreshToken(){
+            return jwt.sign( payload , refreshTokenSecret , {
+                expiresIn : "7d"
+            })
+        } ,
+        forgotPwdToken(){
+            return jwt.sign( payload , forgotPwdTokenSecret , {
+                expiresIn : "3d"
+            })
+        }
+    }
 }
 const verifyToken = (token , type ) => {
-    try{
-        if(type !== "accessToken" 
-            && type !== "refreshToken" 
-            && type !== "forgotPwdToken") 
-        throw new Error(" type token invalid ")
-        return jwt.verify( token , eval(type + "Secret") )
-    } catch (err){
-        throw err ;
+    return {
+        accessToken(){
+            return jwt.verify( token , accessTokenSecret )
+        } ,
+        refreshToken(){
+            return jwt.verify( token , refreshTokenSecret )
+        } ,
+        forgotPwdToken(){
+            return jwt.verify( token , forgotPwdTokenSecret )
+        }
     }
 }
 export { generateTokens , verifyToken}
