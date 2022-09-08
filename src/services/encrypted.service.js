58f7af7d24303743560d1ecead6ecf03a7956a64
dungbt-comartek <dungbt@comartek.com>
2022-09-08 21:57:34 +0700

    refactor encrypt service

--- src/services/encrypted.service.js
+++ src/services/encrypted.service.js
@@ -3,20 +3,12 @@ import bcrypt from "bcrypt" ;
 const saltRounds = 10;
 
 const encryptPwd = async (password) => {
-   try {
     const salt = await bcrypt.genSalt(saltRounds) ;
     return await bcrypt.hash( password , salt) ;
-   } catch (err){
-       throw err ;
-   }
 }
 
 const decodePwd = async (unencryptPwd,pwdEncrypted) => {
-    try {
-     return await bcrypt.compare(unencryptPwd,pwdEncrypted) ;
-    } catch (err){
-        throw err ;
-    }
+    return await bcrypt.compare(unencryptPwd,pwdEncrypted) ;
  }
 
 export  { encryptPwd , decodePwd }
