3a5c0fa0be6d6d14a51ea286359a9d9425a50601
tuannm-comartek <tuannm@comartek.com>
2022-09-08 22:49:58 +0700

    define token error message

--- src/middlewares/verify-token.js
+++ src/middlewares/verify-token.js
@@ -2,10 +2,15 @@ const jwt = require("jsonwebtoken");
 const CONSTANTS = require("../constants");
 const UserRepository = require("../repositories/user.repo");
 
+const TokenErrors = {
+  NotProvided: "Token is not provided",
+  Invalid: "Token is not valid",
+};
+
 const verifyToken = async (req, res, next) => {
   const token = req.token || req.body.token;
   if (!token) {
-    return res.status(404).json({ error: "Token is not provided" });
+    return res.status(404).json({ error: TokenErrors.NotProvided });
   }
 
   try {
@@ -13,11 +18,11 @@ const verifyToken = async (req, res, next) => {
     if (decoded.email) {
       const user = await UserRepository.getUserByEmail(decoded.email);
       if (!user) {
-        return res.stauts(400).json({ error: "Invalid token" });
+        return res.stauts(400).json({ error: TokenErrors.Invalid });
       }
     }
   } catch (error) {
-    return res.status(400).json({ error: "Invalid token" });
+    return res.status(400).json({ error: TokenErrors.Invalid });
   }
 
   next();
