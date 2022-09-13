ca01abd7c28044247ee2c68da17fed0b2559e0b7
giangdv-comartek <giangdv@comartek.com>
2022-09-13 09:48:08 +0700

    refactor: changed mysql-> mongodb

--- src/config/init.js
+++ src/config/init.js
@@ -7,20 +7,7 @@ const mongoose = require('mongoose');
 const init = async () => {
   // init config varibles
   dotenv.config()
-  // init sequelize
-  const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
-    {
-      host: process.env.DATABASE_HOST,
-      dialect: 'mysql',
-      pool: {
-        max: parseInt(process.env.DATABASE_POOL)
-      }
-    },
-  );
-  await mongoose.connect('mongodb://localhost:27017/test');
+  mongoose.connect(process.env.MONGODB);
   if(mongoose.connection.readyState) 
     console.log('connected to mongo database')
   else 
