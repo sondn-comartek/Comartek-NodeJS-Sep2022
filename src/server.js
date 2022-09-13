bd89aa170d6577a9df11721045fbcc6c3f4f5d21
hoangdh-comartek <hoangdh@comartek.com>
2022-09-13 10:26:28 +0700

    Add socket io to chat

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
 import cors from "cors";
+const http = require("http");
+const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
@@ -18,7 +21,48 @@ initWebRoutes(app);
 
 connectDB();
 
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.use((socket, next) => {
+  console.log("handshake first", socket.handshake.query.token);
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(
+      socket.handshake.query.token,
+      process.env.TOKEN_SECRET,
+      (err, decoded) => {
+        if (err) {
+          console.log("Authentication error1");
+          return next(new Error("Authentication error"));
+        }
+        socket.decoded = decoded;
+        next();
+      }
+    );
+  } else {
+    next(new Error("Authentication error"));
+  }
+});
+io.on("connection", (socket) => {
+  // console.log("socket decode:", socket.decoded);
+  console.log("a user connected");
+
+  socket.on("join room", (room) => {
+    socket.join(room);
+    console.log("socket room: ", socket.rooms); // Set { <socket.id>, "room1" }
+  });
+
+  socket.on("chat message", (msg) => {
+    const room = [...socket.rooms][1];
+    io.to(room).emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("backend nodejs is runing on the port " + port);
 });
