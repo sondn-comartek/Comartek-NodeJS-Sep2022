6fa0009d0d6e00421c77b24e29ece822cf3a7358
tuannm-comartek <tuannm@comartek.com>
2022-09-16 17:29:38 +0700

    import passport and jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserEntity } from './entities/user.entity';
+import { PassportModule } from "@nestjs/passport"
+import { JwtModule } from "@nestjs/jwt"
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserEntity }])
+    MongooseModule.forFeature([{ name: User.name, schema: UserEntity }]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION_TIME
+      }
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService]
