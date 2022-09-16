7731287bce60247e388d29c8c09a8ec25a347cbb
tuannm-comartek <tuannm@comartek.com>
2022-09-16 23:44:19 +0700

    add auth service provider in test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthServiceProvider } from './auth.service.spec';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +8,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthServiceProvider],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
+export const mockAuthService = {
+  login: jest.fn(),
+  register: jest.fn()
+}
+
+export const AuthServiceProvider = {
+  provide: AuthService,
+  useValue: mockAuthService
+}
+
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthServiceProvider],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
