571befa0a66584b8efe7774bedde042619ea8d0e
tuannm-comartek <tuannm@comartek.com>
2022-09-17 21:05:23 +0700

    add API swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Add global validation
+  // Global validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // API swagger
+  const config = new DocumentBuilder()
+    .setTitle("Comartek NestJS Shipment API")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(3000);
 }
