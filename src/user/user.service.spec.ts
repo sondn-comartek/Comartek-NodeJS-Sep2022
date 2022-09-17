c7283d08187149fa656a1bb35909a97b39444ca9
tuannm-comartek <tuannm@comartek.com>
2022-09-17 14:20:57 +0700

    add UserServiceProvider in test file

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UserService } from "./user.service";
 
-describe('UserService', () => {
+export const mockUserService = {
+  findUserById: jest.fn(),
+  findUserByEmail: jest.fn(),
+  createUser: jest.fn(),
+};
+
+export const UserServiceProvider = {
+  provide: UserService,
+  useValue: mockUserService,
+};
+
+describe("UserService", () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserServiceProvider],
     }).compile();
 
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
