10e5d03a93e861bf395b8d58fecf63b4cd7a684c
tuannm-comartek <tuannm@comartek.com>
2022-09-18 12:55:48 +0700

    add cors and bodyParser

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bodyParser: true,
+  });
 
   // Global validation
   app.useGlobalPipes(new ValidationPipe());
