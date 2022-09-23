49494bc004e6381f4f2f23a97044acab34335c2e
tuannm-comartek <tuannm@comartek.com>
2022-09-23 11:57:33 +0700

    commit

--- src/pet/pet.resolver.ts
+++ src/pet/pet.resolver.ts
@@ -2,15 +2,16 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreatePetInput } from 'src/shared/inputs';
 import { Pet } from '../shared/schemas/pet.schema';
 import { PetService } from './pet.service';
+import { PetResponseType } from '../shared/types/pet-response.type';
 
 @Resolver()
 export class PetResolver {
-  constructor(private readonly petService: PetService) {}
+  constructor(private readonly petService: PetService) { }
 
-  // @Query(() => [Pet])
-  // async findAllPet(): Promise<Pet[]> {
-  //   return await this.petService.findAllPet();
-  // }
+  @Query(() => [PetResponseType])
+  async findAllPet(): Promise<PetResponseType[]> {
+    return await this.petService.findAllPet();
+  }
 
   // @Query(() => String)
   // async findPetById(): Promise<string> {
